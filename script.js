e9d89b8a357fe1436e19b834c6ad3d64e5107530
TAMJID AHMED <ahmed15-4895@diu.edu.bd>
2024-10-06 20:39:29 +0600

    a

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
-const loadAllPhones=async()=>{
+const loadAllPhones = async (status) => {
     document.getElementById('spinner').classList.add('hidden');
-//     fetch(` https://openapi.programming-hero.com/api/phones?search=iphone`)
-// .then(res=> res.json())
-// .then(data=>console.log(data))
-const response=await fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`);
-const data =await response.json()
-console.log(data.data);
+    //     fetch(` https://openapi.programming-hero.com/api/phones?search=iphone`)
+    // .then(res=> res.json())
+    // .then(data=>console.log(data))
+    const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`);
+    const data = await response.json()
+    if (status)
+
+        displayAllPhones(data.data);
+
+    else
+        displayAllPhones(data.data.slice(0, 6));
+
+
+
 }
 
-function handleSearch(){
-   
+
+
+
+
+
+const displayAllPhones = (phones) => {
+    console.log(phones);
+}
+
+function handleSearch() {
+
     document.getElementById('spinner').classList.remove('hidden')
 
-    setTimeout(function(){
+    setTimeout(function () {
         loadAllPhones()
-    },3000)
+    }, 3000)
 }
 loadAllPhones()
+
+const handleShowALL = () => {
+    loadAllPhones(true)
+
+}
---
+++ test.js
@@ -0,0 +1,18 @@
+const a=()=>{
+    product={
+        name:'iphone',
+        price:3442
+    }
+   
+b(dat)
+}
+
+
+const b=()=>{
+   console.log(phone);
+    
+}
+
+
+
+a()
